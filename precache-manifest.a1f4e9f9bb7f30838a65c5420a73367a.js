97fb97d2ffd7e47db6f11de370606689d401521f
Your Name <bogdan.panin@gmail.com>
2020-10-02 22:52:52 +0300

    Updates

---
+++ precache-manifest.a1f4e9f9bb7f30838a65c5420a73367a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afa172cd86595cb219910941e98c1df",
+    "url": "/react-slider-parallax/index.html"
+  },
+  {
+    "revision": "974adc0b9d69d8947d48",
+    "url": "/react-slider-parallax/static/css/2.3399e8ff.chunk.css"
+  },
+  {
+    "revision": "d24e090a878d2a52e4b8",
+    "url": "/react-slider-parallax/static/css/main.4c9443ac.chunk.css"
+  },
+  {
+    "revision": "974adc0b9d69d8947d48",
+    "url": "/react-slider-parallax/static/js/2.d6c2d46a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-slider-parallax/static/js/2.d6c2d46a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24e090a878d2a52e4b8",
+    "url": "/react-slider-parallax/static/js/main.3d7bd408.chunk.js"
+  },
+  {
+    "revision": "1f595a09eabc74c843dc",
+    "url": "/react-slider-parallax/static/js/runtime-main.5d78f067.js"
+  },
+  {
+    "revision": "b5d0d632486b26c912b4e157d7a03564",
+    "url": "/react-slider-parallax/static/media/germany.b5d0d632.jpg"
+  },
+  {
+    "revision": "e78cb22a7eef79d4a4d15edea06eff80",
+    "url": "/react-slider-parallax/static/media/india.e78cb22a.jpg"
+  },
+  {
+    "revision": "a2e20ccccd4c0d38bf093f553df8d233",
+    "url": "/react-slider-parallax/static/media/israel.a2e20ccc.jpg"
+  },
+  {
+    "revision": "9bf62d06d46b407d3942dd11616ceaef",
+    "url": "/react-slider-parallax/static/media/kiev.9bf62d06.jpg"
+  },
+  {
+    "revision": "d6c2c66e0b17b0008f017b55437d3f2e",
+    "url": "/react-slider-parallax/static/media/usa.d6c2c66e.jpg"
+  }
+]);
